7f6c6227d914147cf7a8493518cba321673c7a6c
zamir23 <zamir.lizardo@utec.edu.pe>
2024-09-29 16:33:50 -0500

    java

---
+++ microservicios-producto-java/src/test/java/com/example/microserviciosproductojava/MicroserviciosProductoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviciosproductojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosProductoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
